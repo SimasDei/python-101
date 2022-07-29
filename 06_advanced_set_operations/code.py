a3bdbb89d7a0755fb07b15aec1038b62a3882144
Simas <simonas.deimantavicius@telesoftas.com>
2022-07-29 15:38:07 +0300

    add advanced set operations and booleans modules

--- 06_advanced_set_operations/code.py
+++ 06_advanced_set_operations/code.py
@@ -5,3 +5,13 @@ abroad = {'Bob', 'Anne'}
 localFriends = friends.difference(abroad)
 
 print(f'Local friends: {localFriends}, the amount of friends is: {len(localFriends)}')
+
+friends = localFriends.union(abroad)
+print(f'Friends: {friends}, the amount of friends is: {len(friends)}')
+
+art = {'Bob', 'Jen', 'Anne', 'Charlie', 'Eve'}
+science = {'Jen', 'Charlie', 'Rolf', 'Adam', 'Gregory'}
+
+studyBothSubjects = art.intersection(science)
+
+print(f'Study both subjects: {studyBothSubjects}, the amount of friends is: {len(studyBothSubjects)}')
---
+++ 07_booleans/code.py
@@ -0,0 +1,20 @@
+# Module which showcases the use of boolean values
+
+equalOperator = 5 == 4 + 1
+notEqualOperator = 5 != 6
+greaterThanOperator = 5 > 2
+lessThanOperator = 5 < 2
+
+print(f'5 == 4 + 1: {equalOperator}')
+print(f'5 != 6: {notEqualOperator}')
+print(f'5 > 2: {greaterThanOperator}')
+print(f'5 < 2: {lessThanOperator}')
+
+friends = ['Rolf', 'Bob', 'Jen', 'Anne']
+abroad = ['Rolf', 'Bob', 'Jen', 'Anne']
+print(f'abroad variable is a reference to friends: {abroad is friends}')
+
+isEqual = friends == abroad
+print(f'Friends == abroad: {isEqual}')
+abroad = friends
+print(f'abroad variable is a reference to friends: {abroad is friends}')
